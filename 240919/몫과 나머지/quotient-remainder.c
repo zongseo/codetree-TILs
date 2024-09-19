72f6b8c4543c04517405fe63ce396053767f207f
zongseo <126531694+zongseo@users.noreply.github.com>
2024-09-19 16:18:27 +0900

    Title: 몫과 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240919/몫과 나머지/quotient-remainder.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // 두 정수를 입력받아서 나눈 몫과 나머지를 출력하는 프로그램을 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d...%d", a/b, a%b);
+    return 0;
+}
