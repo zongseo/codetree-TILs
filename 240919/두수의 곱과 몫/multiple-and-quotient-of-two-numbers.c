5fe3723d3b48af366a2e588ec80811ddfc27f0dc
zongseo <126531694+zongseo@users.noreply.github.com>
2024-09-19 16:17:16 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240919/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // 두 개의 정수를 입력받아 곱과 몫을 출력하는 프로그램을 작성해보세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d * %d = %d\n%d / %d = %d", a, b, a*b, a, b, a/b);
+
+    return 0;
+}
