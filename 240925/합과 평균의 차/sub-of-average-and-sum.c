9294469a34708cab58dd2ee54eb3645ea16bf663
zongseo <126531694+zongseo@users.noreply.github.com>
2024-09-25 15:34:16 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240925/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int d, e, f;
+    d = a+b+c;
+    e = (a+b+c)/3;
+    f = d-e;
+    printf("%d\n%d\n%d", d, e, f);
+    return 0;
+}
