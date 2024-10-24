fb7f48ff95358e4f675d75f1c9b8c711fb9eea00
zongseo <126531694+zongseo@users.noreply.github.com>
2024-10-24 14:26:44 +0900

    Title: 특정 조건 세 정수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
+++ 241024/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
@@ -5,9 +5,9 @@ int main() {
     // 세 개의 수가 모두 같으면 1 아니면 0 을 출력하는 프로그램을 작성해주세요.
     int a, b, c, min;
     scanf("%d %d %d", &a, &b, &c);
-    if(a < b && a < c){min = a;}
-    else if(b < a && b < c){min = b;}
-    else if(c < a && c < b){min = c;}
+    if(a <= b && a <= c){min = a;}
+    else if(b <= a && b <= c){min = b;}
+    else {min = c;}
 
     if(min == a){printf("1 ");}
     else {printf("0 ");}
